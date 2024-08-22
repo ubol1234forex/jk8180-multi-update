21271997e022c67aa29d27556979266a222c42ca
ubol1234forex <ubol1234forex@gmail.com>
2024-08-22 22:15:17 +0700

    Add files via upload

---
+++ moveback.sh
@@ -0,0 +1,4 @@
+rm -rf ~/jk8180-multi/
+cd .. && curl -o- -k https://raw.githubusercontent.com/ubol1234forex/auto-multi/main/update.sh | bash
+mv ~/jk8180-multi-update/name-cpu-all.json ~/jk8180-multi/set-miner-name-cpu-all/
+
